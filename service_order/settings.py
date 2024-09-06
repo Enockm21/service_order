28d87fca357b508ff3b3e0b916046bfb46fb42d0
Enock MUKOKO MAYA <emukoko@planetworkint.com>
2024-09-06 12:41:06 +0200

    add test

--- service_order/settings.py
+++ service_order/settings.py
@@ -108,9 +108,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = 'fr-fr'
 
-TIME_ZONE = "UTC"
+# Set the time zone to Paris, France
+TIME_ZONE = 'Europe/Paris'
 
 USE_I18N = True
 
