c7e2520d47858f193e756c3373b5955e557995a1
Enock MUKOKO MAYA <emukoko@planetworkint.com>
2024-09-06 12:51:13 +0200

    add comment in models

--- order/models.py
+++ order/models.py
@@ -1,15 +1,23 @@
 from django.db import models
 
-# Create your models here.
 class Order(models.Model):
+    """
+    Represents an order made by a customer.
+    """
     customer_id = models.IntegerField()
     order_date = models.DateField()
     total_amount = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
+        """
+        Returns a string representation of the order.
+        """
         return f"Order {self.id} - Customer {self.customer_id}"
 
 class OrderProduct(models.Model):
+    """
+    Represents a product in an order.
+    """
     order_id = models.IntegerField()
     product_id = models.IntegerField()
     quantity = models.IntegerField()
@@ -18,4 +26,8 @@ class OrderProduct(models.Model):
         unique_together = (('order_id', 'product_id'),)
     
     def __str__(self):
+        """
+        Returns a string representation of the order product.
+        """
         return f"Order {self.order_id} - Product {self.product_id}"
+
